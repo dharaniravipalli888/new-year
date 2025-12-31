f64449b154df7d349fdda38327a9fa75e2592e05
dharani ravipalli <100192918+dharaniravipalli888@users.noreply.github.com>
2025-12-31 22:58:53 +0530

    Add files via upload

---
+++ new year 2026/src/components/wish.jsx
@@ -0,0 +1,13 @@
+function Wish({ name }) {
+  return (
+    <div className="wish">
+      <p>
+        Dear <span>{name}</span>, <br />
+        May 2026 bring you success, peace, and happiness 💫 <br />
+        Keep smiling and shining 🎊
+      </p>
+    </div>
+  );
+}
+
+export default Wish;
